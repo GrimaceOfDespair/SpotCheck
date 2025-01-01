79faf04040d667a3440e25faa849b9f9fcc16b06
Igor Kalders <igor@bithive.be>
2025-01-01 19:02:49 +0100

    Patch up SpotCheckPullRequest

--- src/SpotCheckPullRequest/SpotCheckPullRequest.ts
+++ src/SpotCheckPullRequest/SpotCheckPullRequest.ts
@@ -2,6 +2,7 @@ import tl = require('azure-pipelines-task-lib/task');
 import { ToolRunner } from 'azure-pipelines-task-lib/toolrunner';
 import { RobotFileParser } from './Report/RobotFileParser';
 import path from 'node:path';
+import { DiffReportCollector } from './DiffReportCollector';
 
 function throwExpression(errorMessage: string): never {
     throw new Error(errorMessage);
@@ -12,16 +13,23 @@ function throwExpression(errorMessage: string): never {
         const input: string = tl.getInput('input', true) ?? throwExpression(`specify a report file for "input"`);
         const mode = tl.getInput('mode', false) ?? 'robot';
         const baseDir = tl.getInput('baseDir', false) ?? path.dirname(input);
+        const screenshotFolder = tl.getInput('screenshots', false) ?? (path.dirname(input) + '/screenshots');
 
-        let reportFile: string;
+        const diffReportCollector = new DiffReportCollector();
+
+        let output: string;
         switch (mode) {
             case 'robot':
-                reportFile = await new RobotFileParser(input, baseDir)
-                    .parseReport();
+                const diffReport = await new RobotFileParser(input, baseDir)
+                    .createDiffReport(screenshotFolder);
+
+                output = await diffReportCollector.collectReport(baseDir, diffReport);
+
                 break;
 
             case 'cypress':
-                reportFile = input;
+                output = await diffReportCollector.collectReportFile(input);
+
                 break;
 
             default:
@@ -29,9 +37,7 @@ function throwExpression(errorMessage: string): never {
                 break;
         }
 
-        if (reportFile) {
-            tl.uploadArtifact('report', reportFile, 'reportFile');
-        }
+        tl.uploadArtifact('report', output, 'reportFile');
      }
      catch (err:any) {
          tl.setResult(tl.TaskResult.Failed, err.message);
