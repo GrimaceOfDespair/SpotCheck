695b7a3b5fa1d435f051e3d9bb530db38c4cba1a
Igor Kalders <igor@bithive.be>
2025-01-05 03:17:07 +0100

    Minor test refactor

--- src/Tests/SpotCheckPullRequest.test.ts
+++ src/Tests/SpotCheckPullRequest.test.ts
@@ -6,14 +6,20 @@ import path from 'node:path';
 
 describe('SpotCheckPullRequest Suite', function () {
 
+  function createTestRunner(test: string) {
+    const testPath: string = path.join(__dirname, 'task', test);
+    const taskJsonPath: string = path.join(__dirname, '..', 'SpotCheckPullRequest', 'task.json');
+    return new MockTestRunner(testPath, taskJsonPath);
+}
+
   test('Run without arguments', async () => {
-      const testPath: string = path.join(__dirname, 'task', 'empty.ts');
-      const taskJsonPath: string = path.join(__dirname, '..', 'SpotCheckPullRequest', 'task.json');
-      const testRunner = new MockTestRunner(testPath, taskJsonPath);
+
+      const testRunner = createTestRunner('empty.ts');
 
       await testRunner.runAsync();
 
       expect(testRunner.stderr).toBe('');
       expect(testRunner.errorIssues).toEqual(['Input required: input']);
+      
   }, 30 * 1000);
 })
