3ebf7a5d81266101252222ebfee81abad462ccae
Igor Kalders <igor@bithive.be>
2025-01-14 23:46:05 +0100

    Compare differently sized images

--- src/Tests/ImageProcessor.test.ts
+++ src/Tests/ImageProcessor.test.ts
@@ -67,6 +67,28 @@ describe('ImageProcessor', () => {
     expect(percentage).toBe(0);
   });
 
+  test.each([
+    ['larger 8-bit'  , '8bit'          , '8bit-plus10px' ],
+    ['larger 16-bit' , '16bit'         , '16bit-plus10px'],
+    ['smaller 8-bit' , '8bit-plus10px' , '8bit'          ],
+    ['smaller 16-bit', '16bit-plus10px', '16bit'         ],
+  ])('Compare %s image', async (_, baseImage, compareImage) => {
+
+    // Arrange
+    const processor = new ImageProcessor(imageBase);
+
+    // Act
+    const { percentage, testFailed } = await processor.compareImage(
+        `tux-${baseImage}.png`,
+        `tux-${compareImage}.png`,
+        'tux-diff.png',
+        0);
+
+    // Assert
+    expect(testFailed).toBe(false);
+    expect(percentage).toBe(0);
+  });
+
   test('Compare different image adhering threshold', async () => {
 
     // Arrange
@@ -101,4 +123,21 @@ describe('ImageProcessor', () => {
     expect(percentage).toBeGreaterThanOrEqual(.1);
   });
 
+
+  test('Compare with not found diff image', async () => {
+
+    // Arrange
+    const processor = new ImageProcessor(imageBase);
+
+    // Adt
+    const { percentage, testFailed } = await processor.compareImage(
+        'tux-16bit.png',
+        'tux-16bit-notfound.png',
+        'tux-16bit-notfound-diff.png',
+        .1);
+
+    // Assert
+    expect(testFailed).toBe(true);
+    expect(percentage).toBe(1);
+  });
 })
