c27313ada30853712943fee10d520887a635011f
Igor Kalders <igor@bithive.be>
2025-01-14 22:54:45 +0100

    Increase DiffReportCollector coverage

--- src/Tests/DiffReportCollector.test.ts
+++ src/Tests/DiffReportCollector.test.ts
@@ -17,6 +17,63 @@ describe('DiffReportCollector', () => {
             .collectReport(baseDir, report);
 
         // Assert
+        const imageReport = path.join(collectedReportFolder, 'output.json');
+        const imageReportJson = await fs.promises.readFile(imageReport, { encoding: 'utf-8' });
+        
+        expect(imageReportJson).toMatchSnapshot();
+
+        const collectedImageFolder = path.join(collectedReportFolder, 'screenshots');
+        const collectedImages = (await fs.promises.readdir(collectedImageFolder)).map(image =>
+            path.basename(image));
+
+        expect(collectedImages.sort()).toEqual([
+            'Test_Suites.89_Sanitychecks.Listsandstaticsegments.Lists_Dashboard.png',
+            'Test_Suites.89_Sanitychecks.Listsandstaticsegments.Lists_Dashboard_Without_Threshold.png',
+            'baseline']);
+    });
+
+    test('Collect simple report with default path', async () => {
+
+        // Arrange
+        const report = <IDiffTestReport>require('./reports/output-pass.json');
+        const baseDir = path.join(__dirname, 'reports', 'screenshots');
+
+        // Act
+        const collectedReportFolder = await new DiffReportCollector()
+            .collectReport(baseDir, report);
+
+        // Assert
+        const imageReport = path.join(collectedReportFolder, 'diff-report.json');
+        const imageReportJson = await fs.promises.readFile(imageReport, { encoding: 'utf-8' });
+
+        expect(imageReportJson).toMatchSnapshot();
+
+        const collectedImages = (await fs.promises.readdir(collectedReportFolder)).map(image =>
+            path.basename(image));
+
+        expect(collectedImages.sort()).toEqual([
+            'Test_Suites.89_Sanitychecks.Listsandstaticsegments.Lists_Dashboard.png',
+            'Test_Suites.89_Sanitychecks.Listsandstaticsegments.Lists_Dashboard_Without_Threshold.png',
+            'baseline',
+            'diff-report.json']);
+    });
+
+    test('Collect simple report without screenshot folder', async () => {
+
+        // Arrange
+        const report = <IDiffTestReport>require('./reports/output-subfolder.json');
+        const baseDir = path.join(__dirname, 'reports');
+
+        // Act
+        const collectedReportFolder = await new DiffReportCollector('output.json')
+            .collectReport(baseDir, report);
+
+        // Assert
+        const imageReport = path.join(collectedReportFolder, 'output.json');
+        const imageReportJson = await fs.promises.readFile(imageReport, { encoding: 'utf-8' });
+        
+        expect(imageReportJson).toMatchSnapshot();
+
         const collectedImageFolder = path.join(collectedReportFolder, 'screenshots');
         const collectedImages = (await fs.promises.readdir(collectedImageFolder)).map(image =>
             path.basename(image));
@@ -38,6 +95,11 @@ describe('DiffReportCollector', () => {
             .collectReport(baseDir, report);
 
         // Assert
+        const imageReport = path.join(collectedReportFolder, 'output.json');
+        const imageReportJson = await fs.promises.readFile(imageReport, { encoding: 'utf-8' });
+        
+        expect(imageReportJson).toMatchSnapshot();
+
         const collectedImageFolder = path.join(collectedReportFolder, 'screenshots');
         const collectedImages = (await fs.promises.readdir(collectedImageFolder)).map(image =>
             path.basename(image));
